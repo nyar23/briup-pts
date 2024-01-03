b35f25e61b411e587dad2878c322fa8a7571ebc9
CXHZP <96583610+CXHZP@users.noreply.github.com>
2024-01-03 15:18:08 +0800

    更新 Index.vue

--- briup-pts-simple/src/pages/fenceHouse/Index.vue
+++ briup-pts-simple/src/pages/fenceHouse/Index.vue
@@ -273,7 +273,7 @@ export default {
               title: '保存失败',
               message: res.message,
             });
-            toCancel();
+            this.toCancel();
           }
         } else {
           console.log('error submit!!');
