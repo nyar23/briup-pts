3297c435e46229404b5f03e61bfcfc6207bb1fc0
陈杰 <58013910@qq.com>
2024-01-04 16:02:19 +0800

    更新 userManagement.vue

--- briup-pts-simple/src/pages/static-pages/userManagement.vue
+++ briup-pts-simple/src/pages/static-pages/userManagement.vue
@@ -6,6 +6,7 @@
     <div style="background-image: url('http://8.134.137.44:7002/images/2024041704350162775.jpg');">
         <!-- 表头搜索 -->
         <div>
+            <div >用户管理</div>
             <div class="options-div">
                 <el-button type="primary">注册</el-button>
                 <el-button type="danger">批量删除</el-button>
@@ -20,12 +21,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP1111</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：大嘴</div>
+                        <div>电话：11111111-55</div>
+                        <div>会员积分：12322</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352255878.jpeg" alt="">
                         <div>
@@ -35,12 +36,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP2222</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：黑白双煞</div>
+                        <div>电话：22222222-55</div>
+                        <div>会员积分：9949</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352286658.jpeg" alt="">
                         <div>
@@ -50,12 +51,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP3333</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：虾仁</div>
+                        <div>电话：33333333-55</div>
+                        <div>会员积分：9999</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352312082.jpeg" alt="">
                         <div>
@@ -65,12 +66,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP5555</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：魅儿</div>
+                        <div>电话：55555555-55</div>
+                        <div>会员积分：11999</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352350073.jpeg" alt="">
                         <div>
@@ -82,12 +83,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP6666</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：炽焰</div>
+                        <div>电话：66666666-55</div>
+                        <div>会员积分：7879</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352633114.jpeg" alt="">
                         <div>
@@ -97,12 +98,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP7777</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：来福</div>
+                        <div>电话：77777777-55</div>
+                        <div>会员积分：21999</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352406379.jpeg" alt="">
                         <div>
@@ -112,12 +113,12 @@
                 <el-col :span="6">
                     <el-card class="box-card">
                         <div slot="header" class="clearfix">
-                            <span>会员编号：SVIP9999</span>
+                            <span>会员编号：SVIP8888</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：旺财</div>
+                        <div>电话：88888888-55</div>
+                        <div>会员积分：92919</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352436438.jpeg" alt="">
                         <div>
@@ -130,9 +131,9 @@
                             <span>会员编号：SVIP9999</span>
                             <el-button style="float: right; padding: 3px 0" type="text">管理</el-button>
                         </div>
-                        <div>名字：雷龙</div>
-                        <div>电话：11198798798798</div>
-                        <div>会员积分：999</div>
+                        <div>名字：平安</div>
+                        <div>电话：99999999-55</div>
+                        <div>会员积分：89899</div>
                         <img style="width: 250px;height: 250px;"
                             src="http://8.134.137.44:7002/images/2024041704352463371.jpeg" alt="">
                         <div>
